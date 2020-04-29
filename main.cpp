adcc45c84eaac5539e0fcdf81d3407615fe98d63
MADLOUF <61540992+MADLOUF@users.noreply.github.com>
2020-04-29 18:41:01 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -34,11 +34,7 @@ int main()
     Graphe graphe;
     char choix;
     bool quit = 0;
-<<<<<<< HEAD
-    int test=0;
     std::string nomFichier1;
-=======
->>>>>>> 0827701a690069a0fb4c53429481fbed10426b20
     std::string nomFichier2;
 
 
